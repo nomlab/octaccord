e01d31b9346f9b961a960f27bb0c7aa4570153ed
Yoshinari Nomura <nom@quickhack.net>
2014-07-06 18:28:09 +0900

    Scan command to take --search option

--- lib/octaccord/command/scan.rb
+++ lib/octaccord/command/scan.rb
@@ -3,16 +3,20 @@ module Octaccord
     class Scan
       Encoding.default_external = "UTF-8"
 
-      def initialize(client, repos, range = :all, format = :pbl)
-        formatter = Octaccord::Formatter.build(formatter: format)
-        scan(client, repos, range, formatter)
+      def initialize(client, repos, range = :all, **options)
+        formatter = Octaccord::Formatter.build(formatter: options[:format] || :text)
+        scan(client, repos, range, options[:search], formatter)
       end
 
-      def scan(client, repos, range, formatter)
+      def scan(client, repos, range, search, formatter)
         # https://help.github.com/articles/searching-issues
         # or issues = client.list_issues(repos)
         # type:issue means ignore pull request
-        issues = client.search_issues("repo:#{repos} type:issue state:open").items
+
+        query = "repo:#{repos} #{search}"
+        query << " state:open" unless search =~ /state:/
+        query << " type:issue" unless search =~ /type:/
+        issues = client.search_issues(query).items
 
         issues.each do |issue|
           formatter << issue
