ceb04e1fc4fd669a54bfa2a01ba11f27411b595a
Yoshinari Nomura <nom@quickhack.net>
2014-07-09 01:06:16 +0900

    Add --type option to scan command to pick pull-requests

--- lib/octaccord/command/scan.rb
+++ lib/octaccord/command/scan.rb
@@ -5,19 +5,33 @@ module Octaccord
 
       def initialize(client, repos, **options)
         formatter = Octaccord::Formatter.build(formatter: options[:format] || :text)
-        scan(client, repos, options[:search], formatter)
+        if query = options[:search]
+          items = search(client, repos, query, formatter)
+        else
+          items = scan(client, repos, options[:type], formatter)
+        end
+        format(items, formatter)
       end
 
-      def scan(client, repos, search, formatter)
+      def search(client, repos, query, formatter)
         # https://help.github.com/articles/searching-issues
         # or issues = client.list_issues(repos)
         # type:issue means ignore pull request
-
-        query = "repo:#{repos} #{search}"
-        query << " state:open" unless search =~ /state:/
-        query << " type:issue" unless search =~ /type:/
+        query = "repo:#{repos} #{query}"
+        query << " state:open" unless query =~ /state:/
+        query << " type:issue" unless query =~ /type:/
         issues = client.search_issues(query).items
+      end
 
+      def scan(client, repos, type, formatter)
+        if type == "pr"
+          issues = client.issues(repos).select{|issue| issue.pull_request}
+        else
+          issues = client.issues(repos).select{|issue| not issue.pull_request}
+        end
+      end
+
+      def format(issues, formatter)
         issues.each do |issue|
           formatter << issue
         end
