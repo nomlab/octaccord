8988b81a2dddeae6ec5e43ff7186ac01ab115505
Takuya Okada <pitipitiunsyumikan@gmail.com>
2014-07-11 18:17:36 +0900

    Implement get_team_members command

---
+++ lib/octaccord/command/get_team_members.rb
@@ -0,0 +1,18 @@
+module Octaccord
+  module Command
+    class GetTeamMembers
+      Encoding.default_external = "UTF-8"
+
+      def initialize(client, teams, **options)
+        teams.split(',').each do |team|
+          org, team_name = team.split('/')
+          response = client.organization_teams(org)
+          team_id = response.select{|t| t.name == team_name}.first.id
+          response = client.team_members(team_id)
+          members = response.map(&:login)
+          puts members
+        end
+      end
+    end # class Scan
+  end # module Command
+end # module Octaccord
