f93a5f8aa40eb7697bef2154cc809b833c5ad623
Yoshinari Nomura <nom@quickhack.net>
2014-07-19 18:42:11 +0900

    Adjust list formatter

--- lib/octaccord/formatter/list.rb
+++ lib/octaccord/formatter/list.rb
@@ -10,8 +10,27 @@ module Octaccord
       def format_item(issue, options = {})
         header = options[:header] || "*"
         comments = if options[:include_comments] then "\n" + issue.comments else "" end
-        headline = "#{header} #{issue.link} #{issue.status} #{issue.title}#{comments}"
+
+        if issue.references.length > 0
+          refs = " refs:#{issue.references.map{|i| number_link(i)}.join(",")}"
+        else
+          refs = ""
+        end
+
+        if issue.labels != ""
+          labels = " labels:#{issue.labels}"
+        else
+          labels = ""
+        end
+
+        headline = "#{header} #{issue.link} #{issue.status} #{issue.title}#{refs}#{labels}#{comments}"
       end
+
+      private
+      def number_link(number)
+        "[##{number}](../issues/#{number})"
+      end
+
     end # class List
   end # module Formatter
 end # module Octaccord
