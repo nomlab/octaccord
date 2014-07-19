b89d5023aaeb64f56543821fd4d4c84615e5e6b2
Yoshinari Nomura <nom@quickhack.net>
2014-07-19 13:13:34 +0900

    Rename @issue to @resource for a preparation for big change

--- lib/octaccord/formatter/issue.rb
+++ lib/octaccord/formatter/issue.rb
@@ -2,22 +2,22 @@ module Octaccord
   module Formatter
     class Issue
       def initialize(issue)
-        @issue = issue
+        @resource = issue
       end
 
       def status
         return ":parking:" if labels =~ /PBL/
-        # return ":white_check_mark:" if @issue.state == "closed"
-        return ":arrow_upper_left:" if @issue.pull_request
-        return avatar if @issue.assignee
-        days = ((Time.now - @issue.updated_at) / (24*3600)).to_i
+        # return ":white_check_mark:" if @resource.state == "closed"
+        return ":arrow_upper_left:" if @resource.pull_request
+        return avatar if @resource.assignee
+        days = ((Time.now - @resource.updated_at) / (24*3600)).to_i
         return ":ghost:" if days > 60
         return ":grey_question:"
       end
 
       def avatar
-        if @issue.assignee
-          return "![#{@issue.assignee.login}](#{@issue.assignee.avatar_url}s=20)"
+        if @resource.assignee
+          return "![#{@resource.assignee.login}](#{@resource.assignee.avatar_url}s=20)"
         else
           return ":grey_question:"
           octcat = "https://assets-cdn.github.com/images/gravatars/gravatar-user-420.png"
@@ -27,73 +27,73 @@ module Octaccord
       end
 
       def pr
-        if @issue.pull_request then ":arrow_upper_left:" else nil end
+        if @resource.pull_request then ":arrow_upper_left:" else nil end
       end
 
       def references
-        @issue.body.scan(/#(\d+)/).map{|d| d.first.to_i}
+        @resource.body.scan(/#(\d+)/).map{|d| d.first.to_i}
       end
 
       def summary
-        "##{@issue.number} #{@issue.title}" + (labels != "" ? " (#{labels})" : "")
+        "##{@resource.number} #{@resource.title}" + (labels != "" ? " (#{labels})" : "")
       end
 
       def link
-        type = if @issue.pull_request then "pull" else "issues" end
-        "[##{@issue.number}](../#{type}/#{@issue.number} \"#{@issue.title}\")"
+        type = if @resource.pull_request then "pull" else "issues" end
+        "[##{@resource.number}](../#{type}/#{@resource.number} \"#{@resource.title}\")"
       end
 
       def number
-        "#{@issue.number}"
+        "#{@resource.number}"
       end
 
       def plain_link
-        type = if @issue.pull_request then "pull" else "issues" end
-        "[##{@issue.number}](../#{type}/#{@issue.number})"
+        type = if @resource.pull_request then "pull" else "issues" end
+        "[##{@resource.number}](../#{type}/#{@resource.number})"
       end
 
       def comments
         # https://github.com/octokit/octokit.rb#uri-templates
         comments = []
 
-        STDERR.puts "* issue comments: #{@issue.rels[:comments].href}"
+        STDERR.puts "* issue comments: #{@resource.rels[:comments].href}"
 
-        @issue.rels[:comments].get.data.each do |c|
+        @resource.rels[:comments].get.data.each do |c|
           comments << adjust_indent(c.body)
         end
         return comments.join
       end
 
       def title
-        if @issue.state == "closed" then "~~#{@issue.title}~~" else @issue.title end
+        if @resource.state == "closed" then "~~#{@resource.title}~~" else @resource.title end
       end
 
       def labels
-        @issue.labels.map{|label| label.name}.join(',')
+        @resource.labels.map{|label| label.name}.join(',')
       end
 
       def milestone
-        if @issue.milestone then "#{@issue.milestone.title}" else nil end
+        if @resource.milestone then "#{@resource.milestone.title}" else nil end
       end
 
       def created_at
-        @issue.created_at.localtime
+        @resource.created_at.localtime
       end
 
       def updated_at
-        @issue.updated_at.localtime
+        @resource.updated_at.localtime
       end
 
       def story
-        extract_section(@issue.body, "Story")
+        extract_section(@resource.body, "Story")
       end
 
       def demo
-        extract_section(@issue.body, "Demo")
+        extract_section(@resource.body, "Demo")
       end
 
       def cost
-        extract_section(@issue.body, "Cost")
+        extract_section(@resource.body, "Cost")
       end
 
       private
