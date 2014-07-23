6c35516548731baaea2e8955ccf08833b05db76d
Yoshinari Nomura <nom@quickhack.net>
2014-07-23 12:27:18 +0900

    Add new search predicate ``refers''

--- lib/octaccord/command/scan.rb
+++ lib/octaccord/command/scan.rb
@@ -20,7 +20,22 @@ module Octaccord
         query = "repo:#{repos} #{query}"
         query << " state:open" unless query =~ /state:/
         query << " type:issue" unless query =~ /type:/
-        issues = client.search_issues(query).items
+        STDERR.puts "Query: #{query}" if $OCTACCORD_DEBUG
+
+        if query =~ /(-)?refers:\(([^)]+)\)/
+          negop, subquery = $1, $2
+          query = query.sub(/(-)?refers:\([^)]+\)/, "")
+          subquery << " repo:#{repos}"
+          subquery << " state:open" unless subquery =~ /state:/
+          subquery << " type:issue" unless subquery =~ /type:/
+          puts "SUBQUERY: #{subquery}" if $OCTACCORD_DEBUG
+          subitems = client.search_issues(subquery).items
+          puts "QUERY: #{query}" if $OCTACCORD_DEBUG
+          issues = client.search_issues(query).items
+          refers(subitems, issues, (negop == "-"))
+        else
+          issues = client.search_issues(query).items
+        end
       end
 
       def scan(client, repos, type, formatter)
@@ -28,6 +43,40 @@ module Octaccord
           issues = client.issues(repos).select{|issue| issue.pull_request}
         else
           issues = client.issues(repos).select{|issue| not issue.pull_request}
+        end
+      end
+
+      def refers(parents, issues, negop)
+        if $OCTACCORD_DEBUG
+          STDERR.puts "* NEGOP: #{negop}"
+          STDERR.puts "* BEGIN parents ********************************************"
+          STDERR.puts parents.map{|i| i.number}.join(',')
+          STDERR.puts "* END parents ********************************************"
+          STDERR.puts "* BEGIN issues *********************************************"
+          STDERR.puts issues.map{|i| i.number}.join(',')
+          STDERR.puts "* END issues *********************************************"
+        end
+
+        children = []
+
+        parents.each do |parent|
+          STDERR.puts "ABOUT PARENT #{parent.number}:" if $OCTACCORD_DEBUG
+          issues.each do |issue|
+            Octaccord::Formatter::Issue.new(issue).references.each do |refer_num|
+              STDERR.puts "  REFER #{issue.number} refers #{refer_num}" if $OCTACCORD_DEBUG
+              if parent.number.to_i == refer_num
+                STDERR.puts "FOUND #{refer_num}" if $OCTACCORD_DEBUG
+                children << issue
+              end
+            end
+          end
+        end
+        children.uniq!
+
+        if negop
+          return issues - children
+        else
+          return children
         end
       end
 
